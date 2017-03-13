471d742b30f6fa27f325bff96552b1a0bf48c36c
CGP <cgallardo@etsisi.upm.es>
2017-03-13 16:31:49 +0100

    Cambio 1 en comentario

--- hola.cpp
+++ hola.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // Cambio en comentario 1
 	return 0;
 }
